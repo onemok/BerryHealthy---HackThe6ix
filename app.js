c51835d55656f771294966dc412cc029dd95ff08
Emmanuel Sogelola <esogelola@gmail.com>
2022-08-19 21:19:02 -0400

    Downgraded node-fetch

--- app.js
+++ app.js
@@ -1,44 +1,51 @@
-const express = require('express');
+const express = require("express");
+const fetch = require("node-fetch");
+
 const app = express();
 const PORT = 3000;
 app.use(express.json());
-const got = require('got');
-const { pipeline } = require('stream');
 
-const cors = require('cors')
+const { pipeline } = require("stream");
 
-app.use(express.urlencoded({
-    extended: false
-}))
+const cors = require("cors");
+
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
 
 // Apply CORS to all routes
-app.use(cors({ origin: '*'}));
+app.use(cors({ origin: "*" }));
 
-app.get('/', (req, res) => {
-    res.json('This is BerryHealthy!')
+app.get("/", (req, res) => {
+  res.json("This is BerryHealthy!");
 });
 
-const mainpageController = require('./controllers/mainpage_controller.js');
-app.use('/mainpage', mainpageController)
-const loginController = require('./controllers/login_controller.js');
-app.use('/login', loginController)
+const mainpageController = require("./controllers/mainpage_controller.js");
+app.use("/mainpage", mainpageController);
+const loginController = require("./controllers/login_controller.js");
+app.use("/login", loginController);
 
-app.get('/', function(req, res) {
-    const dataStream = got.stream({
-        uri: 'http://www.giantbomb.com/api/search',
-        qs: {
-          api_key: '123456',
-          query: 'World of Warcraft: Legion'
-        }
-    });
-    pipeline(dataStream, res, (err) => {
-        if (err) {
-            console.log(err);
-            res.sendStatus(500);
-        }
-    });
-  });
+app.get("/", function (req, res) {
+  fetch("https://google.com")
+    .then((res) => res.text())
+    .then((text) => console.log(text));
 
+  // const dataStream = got.stream({
+  //     uri: 'http://www.giantbomb.com/api/search',
+  //     qs: {
+  //       api_key: '123456',
+  //       query: 'World of Warcraft: Legion'
+  //     }
+  // });
+  // pipeline(dataStream, res, (err) => {
+  //     if (err) {
+  //         console.log(err);
+  //         res.sendStatus(500);
+  //     }
+  // });
+});
 
 /*
 // This sets up a route to localhost:3000/random and goes off and hits
@@ -60,10 +67,9 @@ app.get('/:apiRoute', async (req, res) => {
 })
 */
 
-
 // Run server
-app.listen(PORT, ()=> {
-    console.log(`App running at http://localhost:${PORT}`)
+app.listen(PORT, () => {
+  console.log(`App running at http://localhost:${PORT}`);
 });
 
 module.exports = app;
